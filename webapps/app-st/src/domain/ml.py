424569f2484ec19e7619ddeb8c1698ec343c1616
sshhtorres <hector.torres@u.icesi.edu.co>
2025-06-24 17:22:07 -0500

    update domain/ml.py

--- webapps/app-st/src/domain/ml.py
+++ webapps/app-st/src/domain/ml.py
@@ -8,21 +8,22 @@ class MLServiceConfig(BaseModel):
 
 
 class MLContainerConfig(BaseModel):
-    envs: dict[str, str] | None = None  # Environment variables for the container, if applicable
-    image: str | None = None  # Container image for the model, if applicable
-    ports: list[int] | None = None  # Ports exposed by the container, if applicable
+    envs: dict[str, str] | None = None  # variables de ambiente; opcional
+    image: str | None  # imagen del contenedor ML; requerido
+    ports: list[int]  # puertos expuestos por el contenedor; requerido con al menos 1 puerto
+    volumes: list[str] | None = None  # volúmenes para el contenedor; opcional; 
 
 
 class MLModelRunningState(BaseModel):
-    endpoints: list[str] | None = None  # Endpoints for the model, if applicable
-    error_message: str | None = None  # Optional error message if the model is unavailable
-    last_updated: str  # ISO format datetime string
-    runner_metadata: dict | None = None  # Metadata about the runner, if applicable
+    endpoints: list[str] | None = None  # endpoints creados si está en ejecución; opcional
+    errors: list[str] | None = None  # errores de estado de ejecución; opcional
+    last_updated: str  # fecha en formato ISO
+    runner_metadata: dict | None = None  # metadata del ejecutor; opcional
     status: str = "unknown"  # "running", "stopped", "unknown"
 
 
 class MLModelConfig(BaseModel):
-    container_config: MLContainerConfig | None = None  # Container image for the model, if applicable
+    container_config: MLContainerConfig
     id: str
     name: str
     runner_id: str
@@ -32,12 +33,12 @@ class MLModelConfig(BaseModel):
 
 class MLRunnerState(BaseModel):
     connected: bool = False
-    error_message: str | None = None  # Optional error message if the model is unavailable
-    last_updated: str  # ISO format datetime string
+    errors: list[str] | None = None  # errores de estado de ejecución; opcional
+    last_updated: str  # fecha en formato ISO
 
 
 class MLRunnerConfig(BaseModel):
     id: str
-    connection: str | None = None
+    connection: str | None = None  # URI de conexión; si es None o str vacío entonces el controlador de ejecutor intentará conexión por defecto
     state: MLRunnerState | None = None
-    type: str  # e.g., "docker", "kubernetes", etc.
+    type: str  # "docker", "kubernetes", "kserve"
